ac891c80a7cb692fb4979b32a4b087e928c8347a
a <a@pop-os.localdomain>
2024-03-01 20:51:59 +0100

    Changed test to reflect new design

--- src/parse.test.ts
+++ src/parse.test.ts
@@ -1,23 +1,30 @@
 import { describe, it, expect } from "@jest/globals";
+import * as E from "fp-ts/Either";
+
 import * as parse from "./parse";
 import { DayOfMonth } from "./dates";
 
+const nanError = E.left("Input must be a number");
+const negativeError = E.left("Input must be non-negative");
+const tooLowError = E.left("Day of month must be at least 1");
+const tooHighError = E.left("Day of month must be at most 31");
+
 describe("dayOfMonth", () => {
   it("should only parse correctly if the day is between 1 and 31", () => {
-    expect(parse.dayOfMonth("dsadh9")).toBeNull();
-    expect(parse.dayOfMonth("0")).toBeNull();
-    expect(parse.dayOfMonth("1")).toEqual(1 as DayOfMonth);
-    expect(parse.dayOfMonth("01")).toEqual(1 as DayOfMonth);
-    expect(parse.dayOfMonth("31")).toEqual(31 as DayOfMonth);
-    expect(parse.dayOfMonth("32")).toBeNull();
+    expect(parse.dayOfMonth("dsadh9")).toEqual(nanError);
+    expect(parse.dayOfMonth("0")).toEqual(tooLowError);
+    expect(parse.dayOfMonth("1")).toEqual(E.right(1));
+    expect(parse.dayOfMonth("01")).toEqual(E.right(1));
+    expect(parse.dayOfMonth("31")).toEqual(E.right(31));
+    expect(parse.dayOfMonth("32")).toEqual(tooHighError);
   });
 });
 
 describe("nonNegative", () => {
   it("should only parse non-negative numbers successfully", () => {
-    expect(parse.nonNegative("9ds 98 ")).toBeNull();
-    expect(parse.nonNegative("-1")).toBeNull();
-    expect(parse.nonNegative("0")).toEqual(0);
-    expect(parse.nonNegative("2983")).toEqual(2983);
-  })
-})
+    expect(parse.nonNegative("9ds 98 ")).toEqual(nanError);
+    expect(parse.nonNegative("-1")).toEqual(negativeError);
+    expect(parse.nonNegative("0")).toEqual(E.right(0));
+    expect(parse.nonNegative("2983")).toEqual(E.right(2983));
+  });
+});
