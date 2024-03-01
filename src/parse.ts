0209a7769ce1a6c251db74e3d5616692ee567823
a <a@pop-os.localdomain>
2024-03-01 15:42:16 +0100

    Started using fp-ts

--- src/parse.ts
+++ src/parse.ts
@@ -1,27 +1,48 @@
+import * as E from "fp-ts/Either";
+
 import * as D from "./dates";
+import { pipe } from "fp-ts/function";
 
 /**
  * Parse a day of month.
  */
-export function dayOfMonth(s: string): D.DayOfMonth | null {
+export function dayOfMonth(s: string): E.Either<string, D.DayOfMonth> {
   const day = nonNegative(s);
-  if (!day || day < 1 || day > D.daysInThisMonth()) {
-    console.log("Please supply a number n, where 1 <= n <= 31");
-    return null;
-  } else {
-    return day as D.DayOfMonth;
-  }
+  return pipe(
+    s,
+    nonNegative,
+    E.chain(
+      E.fromPredicate(
+        (day) => day >= 1,
+        () => "Day of month must be at least 1"
+      )
+    ),
+    E.chain(
+      E.fromPredicate(
+        (day) => day <= D.daysInThisMonth(),
+        () => `Day of month must be at most ${D.daysInThisMonth()}`
+      )
+    ),
+    E.map((day) => day as D.DayOfMonth)
+  );
 }
 
 /**
  * Parse a natural number.
  */
-export function nonNegative(s: string): number | null {
+export function nonNegative(s: string): E.Either<string, number> {
   const n = Number(s);
-  if (isNaN(n) || n < 0) {
-    console.log(`${n} is a negative number.`);
-    return null;
-  } else {
-    return n;
-  }
+  return pipe(
+    n,
+    E.fromPredicate(
+      (n) => !isNaN(n),
+      () => "Input must be a number"
+    ),
+    E.chain(
+      E.fromPredicate(
+        (n) => n < 0,
+        () => "Input must be non-negative"
+      )
+    )
+  );
 }
