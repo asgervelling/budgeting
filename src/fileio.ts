026cd1ae19b36cc7f73da74adc13b42ed7079a6a
a <a@pop-os.localdomain>
2024-03-01 12:35:53 +0100

    Fixed from paths being used

--- src/fileio.ts
+++ src/fileio.ts
@@ -12,27 +12,24 @@ const DATA_DIR = "./data";
  * Read file and create if not exists.
  */
 export function read(filename: string): string {
-  const p = path(filename);
-  createIfNotExists(p);
-  return fs.readFileSync(p, "utf-8");
+  createIfNotExists(filename);
+  return fs.readFileSync(path(filename), "utf-8");
 }
 
 /**
  * Write `data` to the a file.
  */
 export function write(filename: string, data: string): void {
-  const p = path(filename);
-  createIfNotExists(p);
-  fs.writeFileSync(p, data);
+  createIfNotExists(filename);
+  fs.writeFileSync(path(filename), data);
 }
 
 /**
  * Append `data` to the a file.
  */
 export function append(filename: string, data: string): void {
-  const p = path(filename);
-  createIfNotExists(p);
-  fs.appendFileSync(p, data);
+  createIfNotExists(filename);
+  fs.appendFileSync(path(filename), data);
 }
 
 function mkdirIfNotExists(dir: string): void {
@@ -44,7 +41,6 @@ function mkdirIfNotExists(dir: string): void {
 function createIfNotExists(filename: string): void {
   mkdirIfNotExists(DATA_DIR);
   const p = path(filename);
-  console.log("p:", p)
   if (fs.existsSync(p)) return;
   else fs.writeFileSync(p, "");
 }
