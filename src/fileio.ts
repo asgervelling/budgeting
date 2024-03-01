35f2e33c0e9f2384fbdd1a9510b18959e2cc8fd2
a <a@pop-os.localdomain>
2024-03-01 04:33:08 +0100

    documented fileio.ts

--- src/fileio.ts
+++ src/fileio.ts
@@ -1,17 +1,23 @@
+/**
+ * @fileoverview
+ * We use the file system as data storage.
+ * All the functions here make the data directory
+ * if it does not exist.
+ */
 import * as fs from "fs";
 
-const DIR = "./data";
+const DATA_DIR = "./data";
 
 /**
  * Read file and create if not exists.
  */
 export function read(filename: string): string {
-  mkdirIfNotExists(DIR);
+  mkdirIfNotExists(DATA_DIR);
   try {
     return fs.readFileSync(path(filename), "utf-8");
   } catch (error) {
     fs.writeFileSync(filename, "");
-    return fs.readFileSync(filename, "utf-8");
+    return fs.readFileSync(path(filename), "utf-8");
   }
 }
 
@@ -19,7 +25,7 @@ export function read(filename: string): string {
  * Write `data` to the a file.
  */
 export function write(filename: string, data: string): void {
-  mkdirIfNotExists(DIR);
+  mkdirIfNotExists(DATA_DIR);
   try {
     fs.writeFileSync(path(filename), data);
   } catch (error) {
@@ -31,7 +37,7 @@ export function write(filename: string, data: string): void {
  * Append `data` to the a file.
  */
 export function append(filename: string, data: string): void {
-  mkdirIfNotExists(DIR);
+  mkdirIfNotExists(DATA_DIR);
   try {
     fs.appendFileSync(path(filename), data);
   } catch (error) {
@@ -49,5 +55,5 @@ function mkdirIfNotExists(dir: string): void {
  * `"balance" -> "./data/balance"`
  */
 function path(filename: string): string {
-  return `${DIR}/${filename}`;
+  return `${DATA_DIR}/${filename}`;
 }
